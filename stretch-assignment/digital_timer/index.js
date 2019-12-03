db4e5fc208356d545b6cbdbfd98295e1f2519b9a
Chris Engel <Chris.Engel@yahoo.com>
2019-12-03 01:15:56 -0500

    the stretch beat me, raising the white flag for tonight...

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,10 +1,47 @@
 let msTens = document.querySelector('#msTens');
+console.log(msTens);
 let msHundreds = document.querySelector('#msHundreds');
+console.log(msHundreds);
 let secondOnes = document.querySelector('#secondOnes');
-let secondTens = document.querySelector('#secpmdTens');
+console.log(secondOnes);
+let secondTens = document.querySelector('#secondTens');
+console.log(secondTens);
 let digits = document.querySelectorAll('.digit');
+console.log(digits);
+
+// function timer(){
+//     msTens.textContent = 0;
+//     msHundreds.textContent = 0;
+//     secondOnes.textContent = 0;
+//     secondTens.textContent = 0;
+//     let interval = setInterval(function(){
+//         msTens = msTens + 1;
+//         if(msTens >= 10){
+//             msTens = 0;
+//             msHundreds = msHundreds + 1;
+//         };
+//         if(msHundreds >= 10){
+//             msHundreds = 0;
+//             secondOnes = secondOnes + 1;
+//         };
+//         if(secondOnes >= 10){
+//             secondOnes = 0;
+//             secondTens = secondTens +1;
+//         };
+//         if(secondTens >= 10){
+//             digits.className = '.digit .redDigit';
+//             clearTimeout(interval());
+//         };
+//     },10);
+// };
+msTens.textContent = 0;
+msHundreds.textContent = 0;
+secondOnes.textContent = 0;
+secondTens.textContent = 0;
+
+let myTimer = setInterval(timer,10);
 function timer(){
-    msTens.textContent = `${msTens + 1}`
+    msTens = msTens + 1;
     if(msTens >= 10){
         msTens = 0;
         msHundreds = msHundreds + 1;
@@ -18,9 +55,17 @@ function timer(){
         secondTens = secondTens +1;
     };
     if(secondTens >= 10){
-        digits.style.color = 'red';
-        window.clearTimeout();
-        
+        digits.className = '.digit .redDigit';
+        clearTimeout(CB());
     };
+    
 };
-window.setInterval(timer, 10);
+
+const start = document.createElement('button');
+start.textContent = 'start!';
+start.style.display = 'block';
+start.style.margin = '0 auto';
+start.addEventListener('click', myTimer);
+const parentElement = document.querySelector('.digits');
+parentElement.append(start);
+console.log(CB)
