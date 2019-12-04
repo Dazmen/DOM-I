75e1d4b839a427fccc6ce7f579e1d1cc270431dc
Chris Engel <Chris.Engel@yahoo.com>
2019-12-03 19:42:13 -0500

    BOOM digital timer works!

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,71 +1,45 @@
 let msTens = document.querySelector('#msTens');
-console.log(msTens);
 let msHundreds = document.querySelector('#msHundreds');
-console.log(msHundreds);
 let secondOnes = document.querySelector('#secondOnes');
-console.log(secondOnes);
 let secondTens = document.querySelector('#secondTens');
-console.log(secondTens);
 let digits = document.querySelectorAll('.digit');
-console.log(digits);
 
-// function timer(){
-//     msTens.textContent = 0;
-//     msHundreds.textContent = 0;
-//     secondOnes.textContent = 0;
-//     secondTens.textContent = 0;
-//     let interval = setInterval(function(){
-//         msTens = msTens + 1;
-//         if(msTens >= 10){
-//             msTens = 0;
-//             msHundreds = msHundreds + 1;
-//         };
-//         if(msHundreds >= 10){
-//             msHundreds = 0;
-//             secondOnes = secondOnes + 1;
-//         };
-//         if(secondOnes >= 10){
-//             secondOnes = 0;
-//             secondTens = secondTens +1;
-//         };
-//         if(secondTens >= 10){
-//             digits.className = '.digit .redDigit';
-//             clearTimeout(interval());
-//         };
-//     },10);
-// };
-msTens.textContent = 0;
-msHundreds.textContent = 0;
-secondOnes.textContent = 0;
-secondTens.textContent = 0;
+let tens = 0;
+let hundreds = 0;
+let sOnes = 0;
+let sTens = 0;
 
-let myTimer = setInterval(timer,10);
 function timer(){
-    msTens = msTens + 1;
-    if(msTens >= 10){
-        msTens = 0;
-        msHundreds = msHundreds + 1;
-    };
-    if(msHundreds >= 10){
-        msHundreds = 0;
-        secondOnes = secondOnes + 1;
-    };
-    if(secondOnes >= 10){
-        secondOnes = 0;
-        secondTens = secondTens +1;
-    };
-    if(secondTens >= 10){
-        digits.className = '.digit .redDigit';
-        clearTimeout(CB());
-    };
-    
+    let intervalTimer = setInterval(()=>{
+        tens = tens + 1;
+        if(tens >= 10){
+            tens = 0;
+            hundreds = hundreds + 1;
+        };
+        if(hundreds >= 10){
+            hundreds = 0;
+            sOnes = sOnes + 1;
+        };
+        if(sOnes >= 10){
+            sOnes = 0;
+            sTens = sTens +1;
+        };
+        if(sTens === 1){
+            digits.className = '.digit .redDigit';
+            clearInterval(intervalTimer);
+        };
+        msTens.textContent = tens;
+        msHundreds.textContent = hundreds;
+        secondOnes.textContent = sOnes;
+        secondTens.textContent = sTens;
+    },10);
 };
 
 const start = document.createElement('button');
 start.textContent = 'start!';
 start.style.display = 'block';
 start.style.margin = '0 auto';
-start.addEventListener('click', myTimer);
+start.addEventListener('click', timer);
+
 const parentElement = document.querySelector('.digits');
 parentElement.append(start);
-console.log(CB)
